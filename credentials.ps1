59e4ae85d6d9ad3b5c14d2a1806ec8429d5bceba
Akar Singh <aks002@ucsd.edu>
2021-10-24 23:32:18 -0700

    Functions for saving / loading cookies added

--- credentials.ps1
+++ credentials.ps1
@@ -55,3 +55,22 @@ function Get-Plaintext($sec)
 
     return $plaintext;
 }
+
+# Helper function for setting cookie data by writing to file
+#  - written as a secure-string after serializing from a CookieContainer obj
+function Set-Cookies($cookies) {
+    $stream     = [System.IO.MemoryStream]::new();
+    [System.Runtime.Serialization.Formatters.Binary.BinaryFormatter]::new().Serialize($stream, $cookies);
+    $bytes      = [byte[]]::new($stream.Length);
+    $stream.Position = 0;
+    $stream.Read($bytes, 0, $stream.Length);
+    [System.Convert]::ToBase64String($bytes) | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString > 'cookies.dat';
+}
+
+# Helper function for getting cookie data by reading from file
+#  - converted back to plaintext and deserialized back into a CookieContainer ob
+function Get-Cookies() {
+    $bytes  = [System.Convert]::FromBase64String($(Get-Plaintext $(Get-Content 'cookies.dat' | ConvertTo-SecureString)));
+    $stream = [System.IO.MemoryStream]::new($bytes);
+    return [System.Runtime.Serialization.Formatters.Binary.BinaryFormatter]::new().Deserialize($stream);
+}
